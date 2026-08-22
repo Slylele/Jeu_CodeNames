70e609bd1b2f02acb9486bbf590f844e94d949e3
Slylele <57619777+Slylele@users.noreply.github.com>
2026-08-22 14:17:53 +0200

    Refactor app.py for improved readability and structure

--- app.py
+++ app.py
@@ -54,6 +54,7 @@ st.title("CodeNames")
 # if st.button("🔄 Nouvelle grille"):
 #     nouvelle_grille("mots_FR.csv", "Mot")
 #     st.rerun()
+nouvelle_grille("mots_FR.csv", "Mot")
 
 col1, col2, col3 = st.columns([20, 20, 60])
 
